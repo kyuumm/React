c544b178dcaec0419751dddc02d57d76f3fd85e7
kyuumm <kyuuyxx@outlook.com>
2025-12-06 23:06:09 +0800

    DAY3-09-redux美团案例

--- Day3/react-redux/src/App.js
+++ Day3/react-redux/src/App.js
@@ -1,10 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment, addToNum } from "./store/modules/counterStore";
+import { useEffect } from "react";
+import { fetchChannelList } from "./store/modules/channelStore";
 
 function App() {
   const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channel)
   const dispatch = useDispatch()
 
+  //useEffect触发异步请求执行
+  useEffect(() => {
+    dispatch(fetchChannelList())
+  }, [dispatch])
   return (
     <div className="App">
       <button onClick={() => dispatch(decrement())}>-</button>
@@ -12,6 +19,9 @@ function App() {
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
       <button onClick={() => dispatch(addToNum(20))}>add to 20</button>
+      <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
